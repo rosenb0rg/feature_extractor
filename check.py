a86de18098ae19829159243ca81b2b9e87b5004d
rosenb0rg <rosenberg.thomas@gmail.com>
2018-09-28 23:45:24 +0200

    commit

--- check.py
+++ check.py
@@ -3,15 +3,15 @@ import glob
 from char_dir import *
 
 
-name = 'richardson'
+name = 'judgeC'
 scene = 'raupach'
-targ = 10
+targ = '00'
 
 from_n = 1
-to_n = 44
+to_n = 10
 
 # check to see if there are the same number of OBJ and PNG files
-for i in range (from_n, to_n + 1):
+for i in reversed(range(from_n, to_n + 1)):
 	i = '{0:03d}'.format(i)
 	char = Character(name, scene, i, targ)
 	objDir = char.align_obj_dir
@@ -27,11 +27,11 @@ for i in range (from_n, to_n + 1):
 	compedCounter = len(glob.glob1(compedDir, '*.png'))
 	swapCounter = len(glob.glob1(swapDir, '*.png'))
 
-	if not objCounter == pngCounter == cropCounter == convCounter == compedCounter == swapCounter:
-		print "\n", objDir
-		print "obj:", objCounter
-		print "png:", pngCounter
-		print "cropped:", cropCounter
-		print "converted:", convCounter
-		print "comped:", compedCounter
-		print "swapped:", swapCounter
+	# if not objCounter == pngCounter == cropCounter == convCounter == compedCounter == swapCounter:
+	print "\n", objDir
+	print "obj:", objCounter
+	print "png:", pngCounter
+	print "cropped:", cropCounter
+	print "converted:", convCounter
+	print "comped:", compedCounter
+	print "swapped:", swapCounter
--- extractor.py
+++ extractor.py
@@ -59,13 +59,15 @@ args = parser.parse_args()
 #predictor = dlib.shape_predictor("C:/local/src/feature_extractor/shape_predictor_68_face_landmarks.dat")
 
 #input and output directories
+
+
+
+
 scene_name = args.Sce
 character_name = args.Char
 source_number = args.Src
 target_number = args.Targ
 mode_num = args.Mode
-
-
 
 if mode_num == 0:
 	character_info = Character(character_name, scene_name, source_number, target_number)
@@ -88,29 +90,22 @@ if not os.path.exists(out_dir):
 
 save_name = character_info.full_name
 
-print (in_dir, '\n', out_dir)
-
-# in_dir = args.iDir
-# out_dir = args.oDir
+print ("input directory:", in_dir, '\noutput directory:', out_dir)
 
 extensions = ['.png', '.jpg', '.jpeg']
 
 image_path_list = []
-# 00100.png
-# create a list of all the PNG or JPG images in the input directory
-# if file lengh is less than 10 (i.e. hasnt been renamed), then give it a new name w/ correct format
-# (renaming function turned off for now - not useful anymore)
+
+# create a list of all the PNG or JPG images in the input directory, rename if not renamed before
 for extension in extensions:
 	for i, file in enumerate(glob.glob('%s/*%s' % (in_dir, extension))):
 		head, tail = os.path.split(file)
-		print (head, tail)
 		img_num = '{0:03d}'.format(i+1)
-		#print (img_num)
 		renamed = head + '\\' + save_name + '_' + img_num + extension
-		#print ('file:', file, "\n")
-		print ('renamed', renamed, '\n')
-		shutil.move(file, renamed)
-		#image_path_list.append(renamed)
+		if file != renamed:
+			print ('RENAMED!', renamed, '\n')
+			shutil.move(file, renamed)
+		else: print ('already renamed')
 
 for extension in extensions:
 	for i, file in enumerate(glob.glob('%s/*%s' % (in_dir, extension))):
@@ -118,7 +113,7 @@ for extension in extensions:
 
 image_path_list = sorted(image_path_list)
 
-print (image_path_list)
+# print (image_path_list)
 
 # create an empty dictionary for filename, coordinate info
 # to be written to a json file for the replacer script on the other side
@@ -127,84 +122,149 @@ info_dict = {}
 # walk the list of input images, detect images
 detector = MTCNN()
 
-for i, image_path in enumerate(image_path_list):
-	try:
-		# load image and crop out the face with some padding
-		image = cv2.imread(str(image_path))	
-		results = detector.detect_faces(image)[0]
-		#print (results['box'])
-		x, y, w, h = results['box'] 
+if not os.path.isfile('%s/already_cropped.json' % in_dir):
+	with open('%s/already_cropped.json' % in_dir, 'w') as outfile:
+		json.dump('already cropped!', outfile, indent=4)
+		outfile.write("\n")
 
-		pad = int(.2*h)
-		x -= pad
-		w += 2*pad
-		y -= pad
-		h += 2*pad
+	for i, image_path in enumerate(image_path_list):
+		try:
+			image = cv2.imread(str(image_path))	
+			results = detector.detect_faces(image)[0]
+			x, y, w, h = results['box'] 
 
-		image = image[y:y+h, x:x+w]
+			pad = int(.3*h)
+			x -= pad
+			w += 2*pad
+			y -= pad
+			h += 2*pad
+
+			image = image[y:y+h, x:x+w]
 		
-		# write over the exiting image with the cropped one
-		cv2.imwrite(image_path, image)
+			# write over the exiting image with the cropped one
+			cv2.imwrite(image_path, image)
 
-		# redetect mouth points to extract just the mouth
-		mleft = detector.detect_faces(image)[0]['keypoints']['mouth_left']
-		mright = detector.detect_faces(image)[0]['keypoints']['mouth_right']
-		print (image_path)
+			# redetect mouth points to extract just the mouth
+			mleft = detector.detect_faces(image)[0]['keypoints']['mouth_left']
+			mright = detector.detect_faces(image)[0]['keypoints']['mouth_right']
+			print (image_path)
 
-		# determine degrees needed to rotate the image to be aligned
-		degrees = get_rot_angle(mleft, mright)
+			# determine degrees needed to rotate the image to be aligned
+			degrees = get_rot_angle(mleft, mright)
 
-		# rotate original image to align
-		image_r = rotate_image(image, degrees)
+			# rotate original image to align
+			image_r = rotate_image(image, degrees)
 
-		# detect new mouth points (could replace with math)
-		mleft = detector.detect_faces(image_r)[0]['keypoints']['mouth_left']
-		mright = detector.detect_faces(image_r)[0]['keypoints']['mouth_right']
+			# detect new mouth points (could replace with math)
+			mleft = detector.detect_faces(image_r)[0]['keypoints']['mouth_left']
+			mright = detector.detect_faces(image_r)[0]['keypoints']['mouth_right']
 
-		# create coordinates for bounding box
-		w = mright[0] - mleft[0]
-		h = w
-		x = mleft[0]
-		y = mleft[1] - h/2
-		print (x, y, w, h)
+			# create coordinates for bounding box
+			w = mright[0] - mleft[0]
+			h = w
+			x = mleft[0]
+			y = mleft[1] - h/2
+			print (x, y, w, h)
 
-		# add pixels of padding as a percentage of the width
-		pad = int(round(.5*w))
-		x-=pad
-		x=int(x)
-		y-=pad
-		y=int(y)
-		h+=2*pad
-		h=int(h)
-		w+=2*pad
-		w=int(w)
+			# add pixels of padding as a percentage of the width
+			pad = int(round(.6*w))
+			x-=pad
+			x=int(x)
+			y-=pad
+			y=int(y)
+			h+=2*pad
+			h=int(h)
+			w+=2*pad
+			w=int(w)
 
-		coords = [x,y,h,w]
+			coords = [x,y,h,w]
 
-		#basename for files
-		filename = os.path.basename(image_path)
+			#basename for files
+			filename = os.path.basename(image_path)
 
-		# location of the cropped face and source image
-		out_path = '%s/%s' % (out_dir, filename)
-		in_path = '%s/%s' % (in_dir, filename)
+			# location of the cropped face and source image
+			out_path = '%s/%s' % (out_dir, filename)
+			in_path = '%s/%s' % (in_dir, filename)
 
-		# add cropped output file path, coordinates and input 
-		# file path to dictionary
-		info_dict[filename] = coords, degrees, in_path
+			# add cropped output file path, coordinates and input 
+			# file path to dictionary
+			info_dict[filename] = coords, degrees, in_path
 
-		# add padding and resize to 128 pixels
-		roi = image_r[y:y + h,x:x + w]
-		roi = cv2.resize(roi, (128,128))
-		#roi = imutils.resize(roi, 128, inter=cv2.INTER_CUBIC)
+			# add padding and resize to 128 pixels
+			roi = image_r[y:y + h,x:x + w]
+			roi = cv2.resize(roi, (128,128))
+			#roi = imutils.resize(roi, 128, inter=cv2.INTER_CUBIC)
 
-		# write image
-		cv2.imwrite(out_path, roi)
+			# write image
+			cv2.imwrite(out_path, roi)
+		except:
+			pass
+
+if os.path.isfile('%s/already_cropped.json' % in_dir):
+	print ('Already Cropped!')
+	for i, image_path in enumerate(image_path_list):
+		try:
+			image = cv2.imread(str(image_path))
+			# redetect mouth points to extract just the mouth
+			mleft = detector.detect_faces(image)[0]['keypoints']['mouth_left']
+			mright = detector.detect_faces(image)[0]['keypoints']['mouth_right']
+			print (image_path)
+
+			# determine degrees needed to rotate the image to be aligned
+			degrees = get_rot_angle(mleft, mright)
+
+			# rotate original image to align
+			image_r = rotate_image(image, degrees)
+
+			# detect new mouth points (could replace with math)
+			mleft = detector.detect_faces(image_r)[0]['keypoints']['mouth_left']
+			mright = detector.detect_faces(image_r)[0]['keypoints']['mouth_right']
+
+			# create coordinates for bounding box
+			w = mright[0] - mleft[0]
+			h = w
+			x = mleft[0]
+			y = mleft[1] - h/2
+			print (x, y, w, h)
+
+			# add pixels of padding as a percentage of the width
+			pad = int(round(.6*w))
+			x-=pad
+			x=int(x)
+			y-=pad
+			y=int(y)
+			h+=2*pad
+			h=int(h)
+			w+=2*pad
+			w=int(w)
+
+			coords = [x,y,h,w]
+
+			#basename for files
+			filename = os.path.basename(image_path)
+
+			# location of the cropped face and source image
+			out_path = '%s/%s' % (out_dir, filename)
+			in_path = '%s/%s' % (in_dir, filename)
+
+			# add cropped output file path, coordinates and input 
+			# file path to dictionary
+			info_dict[filename] = coords, degrees, in_path
+
+			# add padding and resize to 128 pixels
+			roi = image_r[y:y + h,x:x + w]
+			roi = cv2.resize(roi, (128,128))
+			#roi = imutils.resize(roi, 128, inter=cv2.INTER_CUBIC)
+
+			# write image
+			cv2.imwrite(out_path, roi)
+
+		except:
+			pass
+
+	try:
+		with open('%s/alignments.json' % out_dir, 'w') as outfile:
+			json.dump(info_dict, outfile, indent=4)
+			outfile.write("\n")
 	except:
 		pass
-
-try:
-	with open('%s/alignments.json' % out_dir, 'w') as outfile:
-		json.dump(info_dict, outfile, indent=4)
-		outfile.write("\n")
-except:
-	pass
